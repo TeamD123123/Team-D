762470072b51061591a50f2d6aca4df70db38575
국비지원 수업 < @FULLNAM-2TQ98C8>
2018-08-16 17:16:45 +0900

    MyInfo

---
+++ SpringTeamProject/src/main/java/com/teamD/project/command/BListViewCommand.java
@@ -0,0 +1,13 @@
+package com.teamD.project.command;
+
+import org.springframework.ui.Model;
+
+public class BListViewCommand implements BCommand {
+
+	@Override
+	public void execute(Model model) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ SpringTeamProject/src/main/java/com/teamD/project/command/BMyInfoCommand.java
@@ -0,0 +1,29 @@
+package com.teamD.project.command;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.ui.Model;
+
+import com.teamD.project.BDao.BDao;
+
+public class BMyInfoCommand implements BCommand {
+
+	@Override
+	public void execute(Model model) {
+		// TODO Auto-generated method stub
+		Map<String, Object> map = model.asMap();
+		HttpServletRequest request = (HttpServletRequest)map.get("request");
+		HttpSession session = request.getSession();
+		String name = request.getParameter("mName");
+		String id = request.getParameter("mId");
+		String pwd = request.getParameter("mPwd");
+		String email = request.getParameter("mEmail");
+		String Sid = (String)session.getAttribute("sessionID");
+		System.out.println(Sid);
+		BDao dao = new BDao();
+		dao.myInfo(name, id, pwd, email, Sid);
+	}
+}
